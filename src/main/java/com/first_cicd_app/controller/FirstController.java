4a53c49456234ee59c48f7aeb1649a7f3709620f
Emad Fazal <sk2006606@gmail.com>
2026-09-26 03:18:53 +0530

    test

--- src/main/java/com/first_cicd_app/controller/FirstController.java
+++ src/main/java/com/first_cicd_app/controller/FirstController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
     @GetMapping("/message")
     public String getMessage() {
-        return "Your app is working";
+        return "Your app is working plus boosted";
     }
 }
